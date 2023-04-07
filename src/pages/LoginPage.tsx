0883dc1d433a25a298e15565f751409494538e0d
이홍준 <zunza96@naver.com>
2023-04-08 06:35:59 +0900

    update

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,6 +1,7 @@
 import styled from "@emotion/styled";
 import Button from "components/common/Button";
 import Form from "components/common/Form";
+import {FaGoogle, FaApple} from 'react-icons/fa';
 export function LoginPage() {
   const handleSubmit = (e: React.SyntheticEvent) => {
     e.preventDefault();
@@ -11,8 +12,8 @@ export function LoginPage() {
     <LoginContainer>
       <Form onSubmit={handleSubmit}>
         <img src='/logo.png' alt="logo"/>
-        <Button type="submit">Google로 계속하기</Button>
-        <Button type="submit">Apple로 계속하기</Button>
+        <Button type="submit"><FaGoogle/>Google로 계속하기</Button>
+        <Button type="submit"><FaApple/>Apple로 계속하기</Button>
       </Form>
     </LoginContainer>
   );
